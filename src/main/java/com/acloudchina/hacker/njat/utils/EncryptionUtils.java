5d97c9cdecaa50e459a1ad6af61bc11e00c0c7b2
liuhu <mail.liuhu@gmail.com>
2019-04-17 14:41:00 +0800

    整理工具类实现

--- src/main/java/com/acloudchina/hacker/njat/utils/EncryptionUtils.java
+++ src/main/java/com/acloudchina/hacker/njat/utils/EncryptionUtils.java
@@ -10,6 +10,58 @@ import javax.crypto.spec.SecretKeySpec;
  **/
 public class EncryptionUtils {
 
+
+    /**
+     * 加密字符串
+     * @param str  待加密字符
+     * @param key 秘钥
+     * @return
+     */
+    public static String encrypt2Aes(final String str, String key) {
+        final String s3 = null;
+        final String a = keySubStr(key);
+        key = s3;
+        if (str != null) {
+            if (a == null) {
+                key = s3;
+            }
+            else {
+                key = a;
+                try {
+                    if (a.length() != 16) {
+                        key = keySubStr(a);
+                    }
+                    final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
+                    final Cipher instance = Cipher.getInstance("AES");
+                    instance.init(1, secretKeySpec);
+                    key = byte2Hex(instance.doFinal(str.getBytes("UTF-8"))).toLowerCase();
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                    key = s3;
+                }
+            }
+        }
+        return key;
+    }
+
+    /**
+     * 将16进制字符串解码
+     * @param src
+     * @param key
+     * @return
+     */
+    public static String decodeFromAes(String src, String key) {
+        try {
+            final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+            return new String(cipher.doFinal(hexParseByte2(src)));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     /**
      * 将16进制转换为二进制
      * @param hexStr
@@ -31,24 +83,24 @@ public class EncryptionUtils {
 
     /**
      * 字符串截取 保留16位
-     * @param string
+     * @param key
      * @return
      */
-    private static String a(String string) {
+    private static String keySubStr(String key) {
         String substring;
-        if (string.length() > 16) {
-            substring = string.substring(0, 16);
+        if (key.length() > 16) {
+            substring = key.substring(0, 16);
         }
         else {
-            substring = string;
-            if (string.length() < 16) {
-                int n = string.length() - 1;
+            substring = key;
+            if (key.length() < 16) {
+                int n = key.length() - 1;
                 while (true) {
-                    substring = string;
+                    substring = key;
                     if (n >= 15) {
                         break;
                     }
-                    string += "\u0000";
+                    key += "\u0000";
                     ++n;
                 }
             }
@@ -61,8 +113,8 @@ public class EncryptionUtils {
      * @param array
      * @return
      */
-    public static String a(final byte[] array) {
-        final StringBuffer sb = new StringBuffer(array.length * 2);
+    private static String byte2Hex(final byte[] array) {
+        final StringBuilder sb = new StringBuilder(array.length * 2);
         for (int i = 0; i < array.length; ++i) {
             if ((array[i] & 0xFF) < 16) {
                 sb.append("0");
@@ -70,73 +122,5 @@ public class EncryptionUtils {
             sb.append(Long.toString(array[i] & 0xFF, 16));
         }
         return sb.toString();
-    }
-
-    /**
-     * 加密字符串
-     * @param str  待加密字符
-     * @param key 秘钥
-     * @return
-     */
-    public static String encrypt2Aes(final String str, String key) {
-        final String s3 = null;
-        final String a = a(key);
-        key = s3;
-        if (str != null) {
-            if (a == null) {
-                key = s3;
-            }
-            else {
-                key = a;
-                try {
-                    if (a.length() != 16) {
-                        key = a(a);
-                    }
-                    final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
-                    final Cipher instance = Cipher.getInstance("AES");
-                    instance.init(1, secretKeySpec);
-                    key = a(instance.doFinal(str.getBytes("UTF-8"))).toLowerCase();
-                }
-                catch (Exception ex) {
-                    ex.printStackTrace();
-                    key = s3;
-                }
-            }
-        }
-        return key;
-    }
-
-    /**
-     * 将16进制字符串解码
-     * @param src
-     * @param key
-     * @return
-     */
-    public static String decodeFromAes(String src, String key) {
-        try {
-            return new String(decodeFromAes(hexParseByte2(src), key));
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public static String decodeFromAes2(byte[] content, String key) {
-        try {
-            return new String(decodeFromAes(content, key));
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    private static byte[] decodeFromAes(byte[] content, String key) throws Exception {
-        final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        return cipher.doFinal(content);
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(encrypt2Aes("{\"goodOrderList\":[{\"cardCode\":\"\",\"goodCount\":\"1\",\"goodCutPrice\":\"15\",\"goodFkid\":\"34570075752716613\",\"goodId\":\"203457007574356153000001\",\"goodName\":\"04月16日~11号场-09:00~10:00\",\"goodPrice\":\"15\",\"goodType\":\"1\",\"venueSaleId\":\"903cb7617c284dfc9d2abe6573634c20\"}],\"isCollect\":\"0\",\"orgCode\":\"60fca1582822456bbd5d77f719daca3e\",\"paySource\":\"1\",\"sourceFkId\":\"1124641455777345\",\"sourceName\":\"副馆羽毛球\",\"totalAmount\":\"15.0\",\"totalCutAmount\":\"15.0\",\"userId\":\"34693021860297337\"}", "www.wowsport.cn8"));
     }
 }
