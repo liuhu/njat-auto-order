6200c5498046d5c44079f43f7de9860711392773
liuhu <mail.liuhu@gmail.com>
2019-05-08 14:58:36 +0800

    请求中流控参数deviceId从固定值中获取

--- src/main/java/com/acloudchina/hacker/njat/service/transport/TransportBaseService.java
+++ src/main/java/com/acloudchina/hacker/njat/service/transport/TransportBaseService.java
@@ -8,9 +8,14 @@ import com.acloudchina.hacker.njat.utils.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.LongStream;
 
 /**
  * @description:
@@ -19,22 +24,32 @@ import java.util.UUID;
  **/
 @Slf4j
 public abstract class TransportBaseService {
-    private static final JsonMapper jsonMapper = JsonMapper.alwaysMapper();
-    private static final RequestHeaderDto headerDto = new RequestHeaderDto();
+    private static final JsonMapper JSON_MAPPER = JsonMapper.alwaysMapper();
+    private static final RequestHeaderDto HEADER = new RequestHeaderDto();
+    private static final AtomicLong ADDER = new AtomicLong();
+    /**
+     * 模拟10个终端
+     */
+    private static final Map<Long, String> DEVICE_ID_MAP = LongStream.rangeClosed(0, 9).sorted()
+            .collect(HashMap::new, (m, v) -> m.put(v, UUID.randomUUID().toString()), HashMap::putAll);
 
     @Autowired
     private RestTemplate restTemplate;
 
     public <T> T exchange(Object request, String requestUrl, Class<T> type) {
         RequestDto requestDto = new RequestDto();
-        requestDto.setBody(EncryptionUtils.encrypt2Aes(jsonMapper.toJson(request), GetKeyUtils.getKey()));
+        requestDto.setBody(EncryptionUtils.encrypt2Aes(JSON_MAPPER.toJson(request), GetKeyUtils.getKey()));
         // 突破流控限制
-        headerDto.setDeviceId("8" + UUID.randomUUID().toString());
-        requestDto.setHeader(headerDto);
+        HEADER.setDeviceId("8" + DEVICE_ID_MAP.get(ADDER.incrementAndGet() % (DEVICE_ID_MAP.size())));
+        requestDto.setHeader(HEADER);
 
-        ResponseEntity<String> response = restTemplate.postForEntity(requestUrl, requestDto, String.class);
-        String responseJsonStr = EncryptionUtils.decodeFromAes(response.getBody(), GetKeyUtils.getKey());
-
-        return jsonMapper.fromJson(responseJsonStr, type);
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(requestUrl, requestDto, String.class);
+            String responseJsonStr = EncryptionUtils.decodeFromAes(response.getBody(), GetKeyUtils.getKey());
+            return JSON_MAPPER.fromJson(responseJsonStr, type);
+        } catch (RestClientException e) {
+            log.error("接口调用异常, e = {}", e.getMessage());
+            throw new RuntimeException("接口调用异常!");
+        }
     }
 }
